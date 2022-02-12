e45a8d793d0e87fc75e0340c614786ff620a5b39
CubeStorm <cubestorm72@gmail.com>
2022-02-12 12:43:21 +0100

    Body _document

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -5,7 +5,7 @@ import { Logo } from '@components/nav/Logo';
 
 export const Nav: React.FC = () => {
     return (
-        <nav className="w-full h-14 fixed top-0 left-0 bg-dark-200 py-2 px-4">
+        <nav className="w-full h-14 fixed top-0 left-0 bg-dark-200 shadow-sm py-2 px-4">
             <div className="">
                 <Logo />
             </div>
---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+
+export default function Document() {
+    return (
+        <Html lang="pl">
+            <Head />
+
+            <body className="bg-dark-300">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
