7e0ae0a0a3b80c55e8c5a5a3dfe4e8c7f8adba9c
CubeStorm <cubestorm72@gmail.com>
2022-02-18 19:01:01 +0100

    Missclick

---
+++ hooks/useFriends.ts
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import useSWRInfinite from 'swr/infinite'
+
+import axios from "@lib/axios";
+import { FriendsLists } from "@enums/FriendsType";
+
+import type { UserType } from "@ctypes/features/UserType";
+
+
+export const useFriends = (type: FriendsLists) => {
+    const [isInitialLoading, setIsInitialLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [canFetch, setCanFetch] = useState(true);
+
+    const getKey = (pageIndex: number, previousPageData: []) => {
+        if (previousPageData && !previousPageData.length) return null;
+
+        switch (type) {
+            case FriendsLists.SUGGEST:
+                return `/api/suggests?page=${++pageIndex}`;
+
+            case FriendsLists.INVITES:
+                return `/api/invites?page=${++pageIndex}`;
+
+            default:
+            case FriendsLists.FRIENDS:
+                return `/api/friends?page=${++pageIndex}`;
+        }
+    }
+
+    const fetcher = (url: string) => axios.get(url)
+        .then(response => {
+            setCanFetch(response.data.paginator.current_page !== response.data.paginator.last_page);
+
+            return response.data.paginator.data;
+        })
+        .catch(() => setIsError(true))
+        .finally(() => setIsInitialLoading(false));
+
+    // Fetching data
+    const { data, size, setSize } = useSWRInfinite<UserType[]>(getKey, fetcher);
+
+    useEffect(() => {
+        if (!data) return;
+
+        if (data.length > 0) {
+            setIsInitialLoading(false)
+        }
+
+        setIsLoading(false);
+    }, [data]);
+
+    const loadMore = () => {
+        if (!canFetch) return;
+
+        setIsLoading(true);
+        setSize(size + 1);
+    }
+
+    return {
+        data: data ?? [],
+        isInitialLoading,
+        isLoading,
+        isError,
+        canFetch,
+        loadMore
+    }
+}
