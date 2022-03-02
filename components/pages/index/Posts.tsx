c57f42cfe027eef85e73259e9faf82e794dfccb5
CubeStorm <cubestorm72@gmail.com>
2022-03-02 18:05:59 +0100

    Wider Posts component

--- components/pages/index/Posts.tsx
+++ components/pages/index/Posts.tsx
@@ -4,7 +4,7 @@ import { CreatePost } from '@components/pages/index/CreatePost';
 
 export const Posts: React.FC = () => {
 	return (
-		<div className="max-w-[600px] flex flex-col gap-6 text-black mx-auto p-5">
+		<div className="max-w-[700px] flex flex-col gap-6 text-black mx-auto p-5">
 			<CreatePost />
 		</div>
 	);
