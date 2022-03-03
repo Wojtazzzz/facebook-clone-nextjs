541555155ae21f3671479e0b000f1cfb3c001e4c
CubeStorm <cubestorm72@gmail.com>
2022-03-03 21:11:28 +0100

    More separate

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 
 import { GuestLayout } from '@components/layouts/GuestLayout';
-import { Informations } from '@components/auth/Informations';
 import { Auth } from '@components/auth/Auth';
 
 export default function Login() {
 	return (
 		<GuestLayout>
-			<div className="flex flex-col md:flex-row justify-center gap-16">
-				<Informations />
-				<Auth />
-			</div>
+			<Auth />
 		</GuestLayout>
 	);
 }
