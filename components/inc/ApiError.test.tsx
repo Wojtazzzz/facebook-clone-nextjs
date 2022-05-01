00d6a1b88d37ccf82ed6337033cf61a91cfb5ee4
CubeStorm <cubestorm72@gmail.com>
2022-05-01 12:23:48 +0200

    ApiError tests

---
+++ components/inc/ApiError.test.tsx
@@ -0,0 +1,17 @@
+import { renderWithDefaultData } from '@utils/renderWithDefaultData';
+import { ApiError } from '@components/inc/ApiError';
+import { screen } from '@testing-library/react';
+
+describe('ApiError component', () => {
+    it('render image and text properly', () => {
+        renderWithDefaultData(<ApiError />);
+
+        const image = screen.getByAltText('Server error');
+        const textFirstLine = screen.getByText('Something went wrong');
+        const textSecondLine = screen.getByText('Please try again later');
+
+        expect(image).toBeVisible();
+        expect(textFirstLine).toBeVisible();
+        expect(textSecondLine).toBeVisible();
+    });
+});
