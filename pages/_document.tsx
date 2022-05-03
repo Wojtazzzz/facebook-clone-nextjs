217bf88b522cb1a73faa954cfce1948d6ccc75ec
CubeStorm <cubestorm72@gmail.com>
2022-05-03 21:21:36 +0200

    Wrong html language

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
-        <Html lang="pl">
+        <Html>
             <Head />
 
             <body className="overflow-hidden bg-dark-300">
