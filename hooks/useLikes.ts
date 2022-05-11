3e14f304641c8800d7e80af7251475127b636356
CubeStorm <cubestorm72@gmail.com>
2022-05-11 20:49:46 +0200

    Better argument name

--- hooks/useLikes.ts
+++ hooks/useLikes.ts
@@ -2,15 +2,15 @@ import { useAxios } from '@hooks/useAxios';
 
 import type { LikeResponse } from '@ctypes/responses/LikeResponse';
 
-export const useLikes = (post_id: number) => {
+export const useLikes = (postId: number) => {
     const { state, sendRequest } = useAxios<LikeResponse>();
 
     const handleLike = (isLiked: boolean) => {
         if (state.status === 'LOADING') return;
 
         isLiked
-            ? sendRequest({ method: 'DELETE', url: `/api/likes/${post_id}` })
-            : sendRequest({ method: 'POST', url: '/api/likes', data: { post_id } });
+            ? sendRequest({ method: 'DELETE', url: `/api/likes/${postId}` })
+            : sendRequest({ method: 'POST', url: '/api/likes', data: { postId } });
     };
 
     return { state, handleLike };
