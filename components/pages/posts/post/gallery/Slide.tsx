761fa16cde8b9673c73fee9bdb144ca29840d225
CubeStorm <cubestorm72@gmail.com>
2022-05-19 19:48:34 +0200

    Better image displaying

--- components/pages/posts/post/gallery/Slide.tsx
+++ components/pages/posts/post/gallery/Slide.tsx
@@ -10,7 +10,7 @@ export const Slide = ({ image }: SlideProps) => {
     return (
         <div className="w-full h-full flex justify-center relative">
             <div className="w-2/3 relative">
-                <Image layout="fill" src={getStoredImagePath(image)} objectFit="cover" alt="" />
+                <Image layout="fill" src={getStoredImagePath(image)} objectFit="fill" alt="" />
             </div>
         </div>
     );
