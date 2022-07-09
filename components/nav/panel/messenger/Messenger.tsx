db4187956672fcd52549e522add7178c2ffb1297
CubeStorm <cubestorm72@gmail.com>
2022-07-09 18:27:34 +0200

    CloseOverlay component changed to useOutsideClick hook

--- components/nav/panel/messenger/Messenger.tsx
+++ components/nav/panel/messenger/Messenger.tsx
@@ -1,38 +1,38 @@
 import { memo } from 'react';
 import { useAppDispatch } from '@hooks/redux';
 import { useKey } from '@hooks/useKey';
+import { useOutsideClick } from '@hooks/useOutsideClick';
 
 import { Header } from '@components/nav/panel/inc/Header';
 import { SearchUser } from '@components/nav/panel/messenger/SearchUser';
 import { Messages } from '@components/nav/panel/messenger/Messages';
-import { CloseOverlay } from '@components/inc/CloseOverlay';
 
 import { toggleActive } from '@redux/slices/MessengerSlice';
 
 export const Messenger = memo(() => {
     const dispatch = useAppDispatch();
+
     const handleClose = () => dispatch(toggleActive(false));
+
     useKey('Escape', handleClose);
+    const ref = useOutsideClick(handleClose);
 
     return (
-        <>
+        <div
+            data-testid="messenger-container"
+            ref={ref}
+            className="min-w-[300px] md:min-w-[360px] flex flex-col gap-4 bg-dark-200 absolute top-full -right-24 z-20 shadow-md rounded-md p-3"
+        >
+            <Header testid="messenger-header" title="Messenger" />
+            <SearchUser />
+
             <div
-                data-testid="messenger-container"
-                className="min-w-[300px] md:min-w-[360px] flex flex-col gap-4 bg-dark-200 absolute top-full -right-24 z-20 shadow-md rounded-md p-3"
+                id="list-of-messenger-contacts"
+                className="w-full max-h-[500px] overflow-y-scroll scrollbar-thin scrollbar-thumb-dark-100"
             >
-                <Header testid="messenger-header" title="Messenger" />
-                <SearchUser />
-
-                <div
-                    id="list-of-messenger-contacts"
-                    className="w-full max-h-[500px] overflow-y-scroll scrollbar-thin scrollbar-thumb-dark-100"
-                >
-                    <Messages />
-                </div>
+                <Messages />
             </div>
-
-            <CloseOverlay testid="messenger-overlay" zIndex={10} callback={handleClose} />
-        </>
+        </div>
     );
 });
 
