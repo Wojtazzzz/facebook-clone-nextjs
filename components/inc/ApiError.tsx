8fb3c8748690c05714d81e35923b6bba6e34ee66
CubeStorm <cubestorm72@gmail.com>
2022-08-07 14:48:34 +0200

    Removed needless constants folder

--- components/inc/ApiError.tsx
+++ components/inc/ApiError.tsx
@@ -1,7 +1,5 @@
 import Image from 'next/image';
 
-import { apiErrorStylesProps as stylesProps } from '@constants/ApiErrorStylesProps';
-
 interface ApiErrorProps {
     size?: 'xs' | 'sm' | 'base' | 'lg' | 'xl';
     styles?: string;
@@ -10,17 +8,35 @@ interface ApiErrorProps {
 export const ApiError = ({ size = 'base', styles = '' }: ApiErrorProps) => {
     return (
         <div data-testid="server-error" className={`w-full flex flex-col justify-center items-center my-5 ${styles}`}>
-            <Image
-                width={stylesProps[size].img}
-                height={stylesProps[size].img}
-                src="/img/not_available.svg"
-                alt="Server error"
-            />
+            <Image width={sizes[size].img} height={sizes[size].img} src="/img/not_available.svg" alt="Server error" />
 
             <div className="flex flex-col items-center text-light-100 font-medium mt-4">
-                <span className={`text-${stylesProps[size].text}`}>Something went wrong</span>
-                <span className={`text-${stylesProps[size].text}`}>Please try again later</span>
+                <span className={`text-${sizes[size].text}`}>Something went wrong</span>
+                <span className={`text-${sizes[size].text}`}>Please try again later</span>
             </div>
         </div>
     );
 };
+
+export const sizes = {
+    xs: {
+        img: 50,
+        text: 'xs',
+    },
+    sm: {
+        img: 80,
+        text: 'sm',
+    },
+    base: {
+        img: 110,
+        text: 'base',
+    },
+    lg: {
+        img: 140,
+        text: 'lg',
+    },
+    xl: {
+        img: 170,
+        text: 'xl',
+    },
+} as const;
