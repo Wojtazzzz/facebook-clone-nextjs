f11ca017f8f65d8ac35f38a26fba8c541645ada6
CubeStorm <cubestorm72@gmail.com>
2022-09-16 17:39:03 +0200

    refactor: add custom hook for emojis state managing

--- components/inc/chooseEmoji/ChooseEmoji.tsx
+++ components/inc/chooseEmoji/ChooseEmoji.tsx
@@ -1,22 +1,19 @@
 import * as ReactTooltip from '@radix-ui/react-tooltip';
-import { useState } from 'react';
 import { Emojis } from './emojis/Emojis';
 import { TriggerButton } from './TriggerButton';
+import { useEmojisActive } from './useEmojisActive';
 
 interface ChooseEmojiProps {
     addToContent: (emoji: string) => void;
 }
 
 export const ChooseEmoji = ({ addToContent }: ChooseEmojiProps) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const handleOpen = () => setIsOpen(true);
-    const handleClose = () => setIsOpen(false);
+    const { isActive, open, close } = useEmojisActive();
 
     return (
-        <ReactTooltip.Root open={isOpen} disableHoverableContent={true} delayDuration={0}>
-            <TriggerButton open={handleOpen} />
-            <Emojis addToContent={addToContent} close={handleClose} />
+        <ReactTooltip.Root open={isActive} disableHoverableContent={true} delayDuration={0}>
+            <TriggerButton open={open} />
+            <Emojis addToContent={addToContent} close={close} />
         </ReactTooltip.Root>
     );
 };
