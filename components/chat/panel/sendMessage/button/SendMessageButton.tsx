73727d4f78c0e2776716c26de2075b6b0f2d506f
CubeStorm <cubestorm72@gmail.com>
2022-09-16 17:46:30 +0200

    refactor: remove needless submit action

--- components/chat/panel/sendMessage/button/SendMessageButton.tsx
+++ components/chat/panel/sendMessage/button/SendMessageButton.tsx
@@ -1,10 +1,6 @@
-import { useFormikContext } from 'formik';
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 import { RoundedButton } from '@components/inc/RoundedButton';
-import type { IChatMessagePayload } from '@utils/types';
 
 export const SendMessageButton = () => {
-    const { handleSubmit } = useFormikContext<IChatMessagePayload>();
-
-    return <RoundedButton type="submit" label="Send message" icon={faCircleCheck} callback={handleSubmit} />;
+    return <RoundedButton type="submit" label="Send message" icon={faCircleCheck} callback={() => null} />;
 };
