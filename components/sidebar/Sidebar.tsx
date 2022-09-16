9cba5154db83d72f99bb34de0fde086a62a68109
CubeStorm <cubestorm72@gmail.com>
2022-09-16 15:34:27 +0200

    style: add background color to sidebar

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -10,7 +10,7 @@ export const Sidebar = () => {
     return (
         <aside
             id="sidebar"
-            className="w-full max-w-[220px] xl:max-w-[300px] h-screen overflow-y-auto scrollbar-thin scrollbar-thumb-dark-200 hidden fixed top-0 right-0 lg:flex flex-col mt-14 px-2 pr-4 py-5 pb-16"
+            className="w-full max-w-[220px] xl:max-w-[300px] h-screen bg-dark-300 overflow-y-auto scrollbar-thin scrollbar-thumb-dark-200 hidden fixed top-0 right-0 lg:flex flex-col mt-14 px-2 pr-4 py-5 pb-16"
         >
             <Birthdays />
             <Contacts />
