4145f4b557c2a6f72cf88c83edd9aa1bf7c2f0f6
CubeStorm <cubestorm72@gmail.com>
2022-09-16 15:16:03 +0200

    fix: better posts scrolling

--- components/pages/profile/Profile.tsx
+++ components/pages/profile/Profile.tsx
@@ -10,11 +10,7 @@ interface ProfileProps {
 
 export const Profile = ({ user, friends }: ProfileProps) => {
     return (
-<<<<<<< HEAD
         <div className="w-full max-w-[1200px] flex flex-col gap-5 mx-auto lg:pl-[250px] xl:pl-[300px] box-content">
-=======
-        <div className="w-full max-w-[1200px] flex flex-col gap-5 mx-auto pl-[250px] xl:pl-[300px] box-content">
->>>>>>> cdcf724f534c6ae324e08953970f2e80b11f7e62
             <Hero user={user} friends={friends} />
 
             <div className="w-full flex flex-wrap lg:flex-nowrap justify-center gap-4 p-2">
