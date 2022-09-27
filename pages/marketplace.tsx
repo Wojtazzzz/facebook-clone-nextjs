f0250d94f79a3617efed2604d1823224eca9385c
CubeStorm <cubestorm72@gmail.com>
2022-09-27 12:48:49 +0200

    fix: remove double layout from marketplace

--- pages/marketplace.tsx
+++ pages/marketplace.tsx
@@ -1,15 +1,12 @@
-import { AuthLayout } from '@components/layouts/authLayout/AuthLayout';
 import { MainLayout } from '@components/layouts/authLayout/mainLayout/MainLayout';
 import type { NextPageWithLayout } from '@utils/types';
 import type { ReactElement } from 'react';
 
 const MarketplacePage: NextPageWithLayout = () => {
     return (
-        <AuthLayout>
-            <div className="w-full text-center py-8">
-                <span className="text-4xl text-gray-400 font-medium">Coming soon!</span>
-            </div>
-        </AuthLayout>
+        <div className="w-full text-center py-8">
+            <span className="text-4xl text-gray-400 font-medium">Coming soon!</span>
+        </div>
     );
 };
 
