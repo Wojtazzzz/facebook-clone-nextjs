2c09c4fa3a8207ea0e235d78b9d72a042900dc6c
CubeStorm <cubestorm72@gmail.com>
2022-09-30 11:11:03 +0200

    style: add missing margin to settings

--- components/inc/posts/post/header/settings/Trigger.tsx
+++ components/inc/posts/post/header/settings/Trigger.tsx
@@ -9,7 +9,7 @@ interface TriggerProps {
 export const Trigger = ({ open }: TriggerProps) => {
     return (
         <Popover.Trigger aria-label="Show post settings" onClick={open}>
-            <div className="w-8 h-8 flex justify-center items-center bg-dark-200 hover:bg-dark-100 transition-all rounded-full p-3">
+            <div className="w-8 h-8 flex justify-center items-center bg-dark-200 hover:bg-dark-100 transition-all rounded-full p-3 mr-3">
                 <FontAwesomeIcon icon={faEllipsis} className="text-lg text-light-50" />
             </div>
         </Popover.Trigger>
