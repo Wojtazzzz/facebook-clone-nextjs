4c36ea15128c1cc0cf99ae08d1870d8e761c2be2
CubeStorm <cubestorm72@gmail.com>
2022-10-02 17:21:12 +0200

    refactor: move not found page content to separate component

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,28 +1,10 @@
-import { useRouter } from 'next/router';
-import Image from 'next/future/image';
-import { Button } from '@components/inc/Button';
 import type { NextPageWithLayout } from '@utils/types';
 import type { ReactElement } from 'react';
 import { ErrorLayout } from '@components/layouts/ErrorLayout';
+import { NotFound } from '@components/pages/notFound/NotFound';
 
 const NotFoundPage: NextPageWithLayout = () => {
-    const { push } = useRouter();
-
-    const handleRedirect = () => push('/');
-
-    return (
-        <section className="flex flex-col justify-center items-center">
-            <Image width={112} height={112} src="/img/not_available.svg" alt="Page is not available" />
-
-            <span className="text-lg text-light-100 font-bold text-center mt-2">This page isn&apos;t available</span>
-            <span className="text-base text-light-100 text-center">
-                The link may be broken, or the page may have been removed. Check to see if the link you&apos;re trying
-                to open is correct.
-            </span>
-
-            <Button title="Go to Home Page" styles="w-[300px] mt-8" callback={handleRedirect} />
-        </section>
-    );
+    return <NotFound />;
 };
 
 export default NotFoundPage;
