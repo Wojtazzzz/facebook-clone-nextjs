8c7d1a7e0de54ceb14b0beb759c9305f6db417c3
CubeStorm <cubestorm72@gmail.com>
2022-10-14 09:45:53 +0200

    fix: modal responsibility

--- components/inc/createPost/modal/Modal.tsx
+++ components/inc/createPost/modal/Modal.tsx
@@ -22,9 +22,9 @@ export const Modal = ({ queryKey, close }: ModalProps) => {
                 data-testid="createPostModal"
                 aria-modal="true"
                 aria-labelledby="createPostModal-heading"
-                className="w-full fixed top-1/2 left-1/2 right-auto bottom-auto -translate-y-1/2 -translate-x-1/2 flex flex-col text-light-50 z-50 p-1.5"
+                className="w-full max-w-[520px] min-w-[300px] h-fit max-h-[75vh] fixed top-1/2 left-1/2 right-auto bottom-auto -translate-y-1/2 -translate-x-1/2 box-content flex flex-col text-light-50 z-50 p-1.5"
             >
-                <div className="w-full max-w-[520px] min-w-[300px] h-fit max-h-[75vh] bg-dark-200 box-content rounded-lg">
+                <div className="bg-dark-200 rounded-lg m-2">
                     <Header close={close} />
 
                     <main className="overflow-y-scroll scrollbar-thin scrollbar-thumb-dark-100 scrollbar-track-dark-200">
