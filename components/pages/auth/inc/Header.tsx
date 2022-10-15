f9581b3ead6b51d95e8ab7c522933af84ad7d5e4
CubeStorm <cubestorm72@gmail.com>
2022-10-15 11:28:20 +0200

    feat: change header to more consistent

--- components/pages/auth/inc/Header.tsx
+++ components/pages/auth/inc/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
         <header className="hidden md:flex items-center gap-4 mb-5">
             <Logo size={64} />
 
-            <h1 className="text-3xl md:text-4xl xl:test-5xl text-primary font-bold">Facebook</h1>
+            <h1 className="text-3xl md:text-4xl xl:test-5xl text-primary font-bold">Facebook Clone</h1>
         </header>
     );
 };
