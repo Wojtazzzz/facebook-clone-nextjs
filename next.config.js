fac6fa608cb4cf160038dff8d2b74a9ca75b174d
CubeStorm <cubestorm72@gmail.com>
2022-10-17 20:00:26 +0200

    config: add production app domain to next images config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['localhost', 'images.unsplash.com', 'picsum.photos', 'via.placeholder.com'],
+        domains: ['localhost', 'images.unsplash.com', 'picsum.photos', 'via.placeholder.com', 'api.fb-clone.pl'],
     },
 };
 
