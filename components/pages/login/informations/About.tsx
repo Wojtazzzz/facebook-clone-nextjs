8924420c0826fd4a0a58a11328d6d07f339c835a
CubeStorm <cubestorm72@gmail.com>
2022-10-19 21:43:55 +0200

    refactor: remove mentions about original fb app

--- components/pages/login/informations/About.tsx
+++ components/pages/login/informations/About.tsx
@@ -1,18 +1,8 @@
 export const About = () => {
     return (
         <span className="md:text-lg text-light-100 font-medium">
-            Welcome to Surface App! This is noncommercial and nonofficial version of
-            <a
-                target="_blank"
-                rel="noreferrer"
-                data-testid="informations-facebook_link"
-                href="https://facebook.com/"
-                className="text-white hover:underline mx-1"
-            >
-                Facebook (open in new tab)
-            </a>
-            social app. For safety you cannot create account with own credentials. Instead of you can create free
-            account with random data. Enjoy!
+            Welcome to Surface App! For safety you cannot create account with own credentials. Instead of you can create
+            free account with random data. Enjoy!
         </span>
     );
 };
